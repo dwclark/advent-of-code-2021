e5ed2f8d26ba45abeadd4a69907d2eaf3fd2c9b9
David Clark <plotinussmith@gmail.com>
2021-12-04 14:54:34 -0600

    Better naming

--- src/day-04.lisp
+++ src/day-04.lisp
@@ -18,47 +18,47 @@
 (defparameter *side* 5)
 (defparameter *combos* (concatenate 'list (generate-rows *side*) (generate-cols *side*)))
 
-(defun read-moves (s)
+(defun read-rounds (s)
   (read-from-string (concatenate 'string "(" (substitute #\Space #\, s) ")")))
 
-(defun read-matrices (lst)
+(defun read-bingo-cards (lst)
   (flet ((to-cons (tmp) (mapcar #'(lambda (e) (cons e nil)) tmp)))
     (mapcar #'(lambda (sub)
                 (make-array (* *side* *side*) :initial-contents
                             (to-cons (read-from-string (concatenate 'string "(" sub ")")))))
             lst)))
 
-(defun bingo-round (num matrices)
-  (loop for matrix in matrices
-        do (loop for cell across matrix
+(defun bingo-round (num bingo-cards)
+  (loop for bingo-card in bingo-cards
+        do (loop for cell across bingo-card
                  do (if (= num (car cell))
                         (setf (cdr cell) t)))
-        finally (return matrices)))
+        finally (return bingo-cards)))
 
-(defun sum-unmarked (matrix)
-  (reduce #'+ matrix :key #'(lambda (cell) (if (not (cdr cell)) (car cell) 0))))
+(defun sum-unmarked (bingo-card)
+  (reduce #'+ bingo-card :key #'(lambda (cell) (if (not (cdr cell)) (car cell) 0))))
 
-(defun winner-p (matrix)
+(defun winner-p (bingo-card)
   (find-if (lambda (combo)
-             (= *side* (reduce #'+ combo :key #'(lambda (idx) (if (cdr (aref matrix idx)) 1 0)))))
+             (= *side* (reduce #'+ combo :key #'(lambda (idx) (if (cdr (aref bingo-card idx)) 1 0)))))
            *combos*))
 
 (defun part-1 ()
   (let* ((entries (read-blank-line-blocks "04"))
-         (moves (read-moves (car entries)))
-         (matrices (read-matrices (rest entries))))
-    (loop for num in moves
+         (rounds (read-rounds (car entries)))
+         (bingo-cards (read-bingo-cards (rest entries))))
+    (loop for num in rounds
           do (progn
-               (bingo-round num matrices)
-               (let ((winner (first (remove-if-not #'winner-p matrices))))
+               (bingo-round num bingo-cards)
+               (let ((winner (first (remove-if-not #'winner-p bingo-cards))))
                  (if winner (return-from part-1 (* num (sum-unmarked winner)))))))))
 
 (defun part-2 ()
   (let* ((entries (read-blank-line-blocks "04"))
-         (moves (read-moves (car entries)))
-         (matrices (read-matrices (rest entries))))
-    (loop with remaining = matrices
-          for num in moves
+         (rounds (read-rounds (car entries)))
+         (bingo-cards (read-bingo-cards (rest entries))))
+    (loop with remaining = bingo-cards
+          for num in rounds
           do (progn
                (bingo-round num remaining)
                (let ((winner (first (remove-if-not #'winner-p remaining))))
