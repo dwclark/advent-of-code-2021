33b2cf5cb9f0200999884ce63dfec7da1817f493
David Clark <me@nobody.com>
2023-11-06 23:07:16 -0600

    Shortening code, using print-assert

--- src/day-02.lisp
+++ src/day-02.lisp
@@ -1,35 +1,23 @@
-(defpackage :day-02
-  (:use :cl)
-  (:import-from :utils :read-day-file)
-  (:export #:part-1 #:part-2))
-
+(defpackage :day-02 (:use :cl)
+  (:import-from :utils :read-day-file :print-assert)
+  (:export #:exec))
 (in-package :day-02)
 
-(defun eval-file (func)
-  (dolist (line (read-day-file "02"))
-    (let ((lst (read-from-string (concatenate 'string "(" line ")"))))
-      (apply func lst))))
+(defun part-1 (commands)
+  (loop for (sym by) in commands
+	sum (if (eq sym 'forward) by 0) into horizontal
+	sum (if (eq sym 'down) by (if (eq sym 'up) (- by) 0)) into depth
+	finally (return (* horizontal depth))))
 
-(defun part-1 ()
-  (let ((horizontal 0)
-        (depth 0))
-    (eval-file #'(lambda (sym x)
-                   (case sym
-                     (forward (incf horizontal x))
-                     (down (incf depth x))
-                     (up (decf depth x)))))
-    (* horizontal depth)))
-      
+(defun part-2 (commands)
+  (loop for (sym by) in commands
+	sum (if (eq sym 'down) by (if (eq sym 'up) (- by) 0)) into aim
+	sum (if (eq sym 'forward) by 0) into horizontal
+	sum (if (eq sym 'forward) (* aim by) 0) into depth
+	finally (return (* horizontal depth))))
 
-(defun part-2 ()
-  (let ((horizontal 0)
-        (depth 0)
-        (aim 0))
-    (eval-file #'(lambda (sym x)
-                   (case sym
-                     (down (incf aim x))
-                     (up (decf aim x))
-                     (forward
-                      (incf horizontal x)
-                      (incf depth (* aim x))))))
-    (* horizontal depth)))
+(defun exec ()
+  (labels ((parse (line) (read-from-string (concatenate 'string "(" line ")"))))
+    (let ((commands (mapcar #'parse (read-day-file "02"))))
+      (print-assert "Part 1:" (part-1 commands) 1989014)
+      (print-assert "Part 2:" (part-2 commands) 2006917119))))
